eadfba24944741ed9c06c5d81665f3e281455708
IzabelyBrumR <103642004+IzabelyBrumR@users.noreply.github.com>
2024-03-19 09:44:12 -0300

    Add files via upload

---
+++ lojaIza/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <link href="css/maxcdn.bootstrapcdn.com_bootstrap_3.4.1_css_bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <script src="js/ajax.googleapis.com_ajax_libs_jquery_3.6.4_jquery.min.js" type="text/javascript"></script>
+        <script src="js/maxcdn.bootstrapcdn.com_bootstrap_3.4.1_js_bootstrap.min.js" type="text/javascript"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+        <style>
+           
+        </style>
+    </head>
+    <?php
+        require_once './shared/header.php';
+    ?>
+    <body class="fundo">
+        
+    </body>
+</html>
